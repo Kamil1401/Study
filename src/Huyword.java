9799a4bec4c98f6baa1e76fe52339f037fe953a5
captain-kam@yandex.ru <captain-kam@yandex.ru>
2023-12-15 15:22:17 +0400

    "Хуёвое" задание выполнено =)

---
+++ src/Huyword.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Huyword {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите ваше слово");
+            String word = scanner.nextLine();
+            System.out.println("Хуй" + word + ".");
+        }
+    }
+}
